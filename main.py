0a04703fb1a5cd2d9540fd488967bf9489f737b3
nnmaki <teamnemo78@gmail.com>
2024-08-13 17:29:44 +0300

    Upload img:s and try place castle

--- main.py
+++ main.py
@@ -8,15 +8,27 @@ pygame.init()
 # game window
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+clock = pygame.time.Clock()
+FPS = 60
 
 # create game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Ćastle Game')
 
+# load images - background
+bg = pygame.image.load('img/bg.png').convert_alpha()
+# load images - castle
+castle_img_100 = pygame.image.load('img/castle/castle_100.png').convert_alpha()
+
+
 # game loop
 run = True
 while run:
 
+    clock.tick(FPS)
+
+    screen.blit(bg, (0, 0))
+    screen.blit(castle_img_100, (0, 0))
 
 
     # event handler
@@ -24,6 +36,8 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    # update display window
+    pygame.display.update()
 
 pygame.quit
 
